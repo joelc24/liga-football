ad0fe77b37ecaffda0ab2ea8130e8cd0c34d86b3
joel <libra-marriaga@hotmailt.com>
2023-06-02 21:11:47 -0500

    cambio en la conexion

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -2,9 +2,10 @@ import { Sequelize } from 'sequelize';
 
 const username = process.env.DB_USERNAME ?? 'root';
 const database = process.env.DB_DATABASE ?? 'liga';
+const dbhost = process.env.DB_HOST ?? 'localhost';
 
 export const sequelize = new Sequelize(database, username, '', {
-  host: 'localhost',
+  host: dbhost,
   dialect: 'mysql'
 });
 
